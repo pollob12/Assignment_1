8b3f1778b59593d86f09b9aa6378f09be136958c
pollob12 <85349658+pollob12@users.noreply.github.com>
2023-05-09 18:40:55 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// Send data to the Console log 
+
+console.log("we can send date to consol log");   
+
+// Create an error and warning message to console
+
+console.warn('this is warn message'); 
+console.error('this is earror conslog');
+
+// create a prompt and get user name and age 
+
+prompt('please type your name')
+prompt('please type your age')
+
+// we can taka data form user using promt in consol log 
+console.log('my name is'+ prompt("type your name") +'and i love to play '+ prompt("what is your favourite sports"));
